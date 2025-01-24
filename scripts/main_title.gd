cb95d52301951c134d6ce4e7b807107dbd8e579e
Azrotho <contact@azrotho.fr>
2025-01-24 21:42:37 +0100

    fix

--- scripts/main_title.gd
+++ scripts/main_title.gd
@@ -7,12 +7,6 @@ extends Control
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_packed(game)
 
