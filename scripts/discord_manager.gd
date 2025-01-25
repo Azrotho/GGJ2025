b9a2a0eb3f7f538e0f59efc10a75aa0c18e6a9b0
Azrotho <contact@azrotho.fr>
2025-01-25 10:47:02 +0100

    update

--- scripts/discord_manager.gd
+++ scripts/discord_manager.gd
@@ -5,7 +5,7 @@ extends Node
 func _ready():
 	DiscordRPC.app_id = 1332642844766371963
 	DiscordRPC.details = "Playing FishyFish!"
-	DiscordRPC.state = ":D"
+	DiscordRPC.state = "in the bubble"
 	DiscordRPC.start_timestamp = int(Time.get_unix_time_from_system())
 	DiscordRPC.refresh()
 
