247a99e8692bbb53f424950809f7494ead2ba35a
Azrotho <contact@azrotho.fr>
2025-01-26 10:29:15 +0100

    polish end

--- scripts/final_scene.gd
+++ scripts/final_scene.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 @onready var timeText = $Camera2D/Label3
+@onready var button = $Camera2D/Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	button.grab_focus()
 	var time = Globals.speedrun_time
 	var formatted_time = str(time)
 	var decimal_index = formatted_time.find(".")
@@ -14,3 +16,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/MainTitle.tscn")
